c18c4c205891eed78dfca86302f2417cda687d5f
Gregg Fisher <gregg@quikklymobile.com>
2016-12-23 11:58:59 +0000

    added auto-layout snippet for initial rendering.

--- LocalSocialSharing/app/src/main/java/android/quikkly/net/samples/ProfileActivity.java
+++ LocalSocialSharing/app/src/main/java/android/quikkly/net/samples/ProfileActivity.java
@@ -1,9 +1,11 @@
 package android.quikkly.net.samples;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,12 +16,13 @@ import net.quikkly.android.render.Skin;
 import net.quikkly.java.scan.Scanner;
 
 /**
- * Created by fisher on 15/12/2016.
+ * Created by Gregg Fisher on 15/12/2016.
  */
 
 public class ProfileActivity extends AppCompatActivity {
 
     private ScannableImageView mScannableImageView;
+    private Skin mSkin;
     private TextView tvUsername;
     private TextView tvName;
     private TextView tvGender;
@@ -39,8 +42,8 @@ public class ProfileActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         Long userNum = intent.getLongExtra("userNum", -1);
-        User user = ProfileHelper.getInstance().getUser(userNum);
 
+        User user = ProfileHelper.getInstance().getUser(userNum);
         if(user != null) {
 
             Scanner mScanner = new Scanner();
@@ -50,27 +53,41 @@ public class ProfileActivity extends AppCompatActivity {
             Skin.ViewBox viewBox = new Skin.ViewBox();
             viewBox.setWidth(358.0D);
             viewBox.setHeight(358.0D);
-            viewBox.setX(65.0D);
+            viewBox.setX(80.0D);
             viewBox.setY(65.0D);
 
-            Skin skin = new Skin();
-            skin.setCodePattern(codePattern);
-            skin.setLayout(codeLayout.ordinal() + 1);
-            skin.setVersion(3);
-            skin.setBorderColour("#FFFFFF");
-            skin.setCodeDotColor("#FFFFFF");
-            skin.setBackgroundColor("#000000");
-            skin.setFooter("");
-            skin.setShowFooter(false);
-            skin.setLogoWidth(300.0D);
-            skin.setLogoHeight(300.0D);
-            skin.setLogoScaleFactor(0.75D);
-            skin.setLogoViewBox(viewBox);
-            skin.setTextColor("#000000");
-            skin.setLogoUri(user.getProfilePic());
+            mSkin = new Skin();
+            mSkin.setCodePattern(codePattern);
+            mSkin.setLayout(codeLayout.ordinal() + 1);
+            mSkin.setVersion(3);
+            mSkin.setFooter("");
+            mSkin.setShowFooter(false);
+            mSkin.setBorderColour("#FFFFFF");
+            mSkin.setCodeDotColor("#FFFFFF");
+            mSkin.setBackgroundColor("#5a47ec");
+            mSkin.setLogoWidth(200.0D);
+            mSkin.setLogoHeight(200.0D);
+            mSkin.setLogoScaleFactor(1D);
+            mSkin.setLogoViewBox(viewBox);
+            mSkin.setTextColor("#000000");
+            mSkin.setLogoUri(user.getProfilePic());
 
-            ScannableImageView scannableImageView = (ScannableImageView) super.findViewById(R.id.scannable_view);
-            scannableImageView.render(skin, true, R.drawable.default_user); // Provide a default image to show in place of the URI whilst the actual image is genrated from the cache, disk or via download.
+            mScannableImageView = (ScannableImageView) super.findViewById(R.id.scannable_view);
+            mScannableImageView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                @Override
+                public void onGlobalLayout() {
+
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                        // noinspection deprecation
+                        mScannableImageView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                    }
+                    else {
+                        mScannableImageView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    }
+
+                    mScannableImageView.render(mSkin, true, R.drawable.default_user); // Provide a default image to show in place of the URI whilst the actual image is genrated from the cache, disk or via download.
+                }
+            });
 
             if(tvUsername != null)
                 tvUsername.setText(user.getUsername());
